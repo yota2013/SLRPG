0f29a291713292ab03aa5c8800b1f4b5c6b2d023
domitoo <domi.arc@gmail.com>
2017-06-30 00:19:27 +0900

    ignore update install 2d texture folder remake

---
+++ Assets/Scripts/Battle/CharacterState.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CharacterState {
+	private GameObject character;
+	private int HP;
+	private int move;
+	private int AGL;
+	private int DEF;
+
+	public CharacterState(int hp,int move,int agl,int def){
+		this.HP = hp;
+		this.move = move;
+		this.AGL = agl;
+		this.DEF = def;
+	}
+}
---
+++ Assets/Scripts/Battle/MapChip.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MapChip {
+	private GameObject mapChipObj;
+	private bool isCharacter;
+	private int spriteNum;
+	private int x, y;
+    private Vector2 pos;
+	public MapChip(int x, int y, GameObject mapchip){
+		spriteNum = 0;
+		isCharacter = false;
+        pos.x = x;
+        pos.y = y;
+		mapChipObj = mapchip;
+	}
+
+	public int getspriteNum(){
+		return spriteNum;
+	}
+
+    public Vector2 getPosition() {
+        return pos;
+    }
+
+	public void printState(){
+		Debug.Log (this.x+" "+this.y);
+	}
+}
