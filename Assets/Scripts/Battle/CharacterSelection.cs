bb4451dd5865996351bfb6bc8e13f367b3abff91
LAPTOP-KBAAL0E2\IS <is0202iv@ed.ritsumei.ac.jp>
2017-08-11 14:11:00 +0900

    CharacterSelection UIなし版 GameManagerのSerializeField変更

---
+++ Assets/Scripts/Battle/CharacterSelection.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * 自軍出撃キャラ選択
+ **/
+public class CharacterSelection{
+
+	List<long> selectedCharaList; //キャラリスト
+	private const short character_max = 2;
+
+	public IEnumerator SelectCharacter()
+	{
+		GameManager.Instance.enabled = false;
+		Debug.Log ("Select Character");
+		Debug.Log ("GUIなし　Push 1 & 2");
+		selectedCharaList = new List<long> ();
+		//選択処理
+		while(true)
+		{
+			//Listに追加する処理
+			if (Input.GetKeyDown ("1")) 
+			{
+				addCharacter (1345010301L);
+			} else if(Input.GetKeyDown ("2"))
+			{
+				addCharacter (1145010302L);
+			}else if(Input.GetKeyDown ("3"))
+			{
+				addCharacter (1145010302L);
+			}else if(Input.GetKeyDown ("4"))
+			{
+				addCharacter (1023010301L);
+			}
+			if (selectedCharaList.Count == character_max) {
+				endSelection ();
+				yield break;
+			}
+			yield return null;
+		}
+	}
+
+	public void addCharacter(long index)
+	{
+		if (!selectedCharaList.Contains (index)) 
+		{
+			selectedCharaList.Add (index);
+			Debug.Log ("Add Character :"+index);
+		}
+	}
+
+	private void endSelection()
+	{
+		
+		//GameManager_temp_ito.Instance.moveCharacter = this.gameObject.AddComponent<MoveCharacter> ();
+		//キャラ配置*/
+		GameManager.Instance.CharacterArrengement(selectedCharaList, true);
+
+		GameManager.Instance.enabled = true;
+		Debug.Log ("選択終了");
+	}
+
+}
--- Assets/Scripts/State/CharacterInfo.cs
+++ Assets/Scripts/State/CharacterInfo.cs
@@ -17,12 +17,20 @@ public class CharacterInfo : MonoBehaviour {
 	public CharacterInfo(long chara_id){
 		InstantiateCharacter (chara_id);
     }
+	public void Initialize(long chara_id)
+	{
+		InstantiateCharacter (chara_id);
+	}
 
 	private void InstantiateCharacter(long id){
 //      string json = JsonToString.loadFromFile(config_dir, id.ToString());
 //		TODO: specify character ID on instantiate
-		string json = JsonToString.loadFromFile("Config/Character", "1345010201");
+		string json = JsonToString.loadFromFile("Config/Character", id.ToString());
 		chara = JsonUtility.FromJson<Character> (json);
+		SpriteRenderer spriteRenderer = this.gameObject.GetComponent<SpriteRenderer> ();
+		Sprite[] sp = Resources.LoadAll<Sprite> ("Character/" + id.ToString());
+		spriteRenderer.sprite = System.Array.Find<Sprite> (sp,(sprite) => sprite.name.Equals(id.ToString()+"_0"));
+
 	}
 
 	public string name{
